c3bdf2bd38a6152c82324960f9ee8e4588ee9462
deniallugo <deniallugo@gmail.com>
2018-11-25 21:01:20 +0300

    Add error cheching and readme

--- views.py
+++ views.py
@@ -1,38 +1,39 @@
 from aiohttp import web
 
-from graph import FindType, SortType
+from graph import FindType, DirectionType
 from serializers import serialize
 
 
 async def view_best_path(request):
-    source = request.query.get('source')
-    dest = request.query.get('dest')
+    source = request.match_info.get('source')
+    dest = request.match_info.get('destination')
     trip_type = request.query.get('trip_type', FindType.best)
-    sort_type = request.query.get('sort', SortType.asc)
-    if source and dest:
-        graph = request.app['graph']
+    direction_type = request.query.get('sort', DirectionType.asc)
 
+    graph = request.app['graph']
+    try:
         trip = await request.app.loop.run_in_executor(
-            None, graph.find_best_trip, source, dest, trip_type, sort_type)
-
+            None, graph.find_best_trip, source, dest, trip_type,
+            direction_type)
+    except KeyError as e:
+        response = {'error': f'{e.args[0]} not find'}
+    else:
         response = list(map(serialize, trip))
 
-        return web.json_response(response)
-    else:
-        raise web.HTTPBadRequest(text='dest and source params not provided')
+    return web.json_response(response)
 
 
 async def view_all_paths(request):
-    source = request.query.get('source')
-    dest = request.query.get('dest')
-    if source and dest:
-        graph = request.app['graph']
+    source = request.match_info.get('source')
+    dest = request.match_info.get('destination')
 
+    graph = request.app['graph']
+    try:
         all_paths = await request.app.loop.run_in_executor(
             None, graph.find_all_path, source, dest)
-
+    except KeyError as e:
+        response = {'error': f'{e.args[0]} not find'}
+    else:
         response = list(map(serialize, all_paths))
 
-        return web.json_response(response)
-    else:
-        raise web.HTTPBadRequest(text='dest and source params not provided')
+    return web.json_response(response)
